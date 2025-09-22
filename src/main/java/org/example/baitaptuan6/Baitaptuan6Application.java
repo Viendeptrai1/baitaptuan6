d9b736951ad61e870d0015ac4baadccc994fb86b
Vien Phan <phanquocvien.123@gmail.com>
2025-09-22 12:14:21 +0700

    first commit

---
+++ src/main/java/org/example/baitaptuan6/Baitaptuan6Application.java
@@ -0,0 +1,13 @@
+package org.example.baitaptuan6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baitaptuan6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baitaptuan6Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/baitaptuan6/Baitaptuan6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.baitaptuan6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baitaptuan6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
